fdce3c50c75d71fbcd8e24227cc852718230231c
Raul Incze <raulincze@gmail.com>
2015-11-14 17:47:27 +0200

    Added locomotive model and tweens.

---
+++ ubi2015_unity/Ubi2015/Assets/Scripts/Tweens/RotationTween.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class RotationTween : MonoBehaviour
+{
+    public Vector3 amount;
+    public Ease easeType = Ease.Linear;
+    public LoopType loopType = LoopType.Restart;
+    public float rotationTime = 1f;
+	// Use this for initialization
+	void Start ()
+    {
+        transform.DOBlendableLocalRotateBy(amount, rotationTime).SetLoops(-1, loopType).SetEase(easeType);
+	}
+}
---
+++ ubi2015_unity/Ubi2015/Assets/Scripts/Tweens/ScaleTween.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ScaleTween : MonoBehaviour {
+    public Vector3 scaleTarget;
+    public Ease easeType = Ease.Linear;
+    public LoopType loopType = LoopType.Restart;
+    public float tweenTime = 1f;
+    // Use this for initialization
+    void Start()
+    {
+        transform.DOScale(scaleTarget, tweenTime).SetLoops(-1, loopType).SetEase(easeType);
+    }
+}
