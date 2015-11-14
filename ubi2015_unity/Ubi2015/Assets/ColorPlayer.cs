0d0f6a40cf134ee88db471427961a1a648b19662
Flaviu <flaviuvescan@gmail.com>
2015-11-14 18:03:02 +0200

    Main menu

---
+++ ubi2015_unity/Ubi2015/Assets/ColorPlayer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorPlayer : MonoBehaviour {
+
+    public Color[] colors;
+    public RawImage[] playerImages;
+
+    private int activePlayers = 0;
+
+    public GameObject playerSelection;
+    public GameObject difficultySelection;
+
+    public enum MainMenuStates { selectingCharacter, selectingDifficulty };
+    public MainMenuStates currentState = MainMenuStates.selectingCharacter;
+
+    private KeyCode difficultyKey;
+    private KeyCode enterGameKey;
+    private KeyCode startGameKey;
+
+    void Update()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            enterGameKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + (i + 1).ToString() + "Button0");
+
+            startGameKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + (i + 1).ToString() + "Button7");
+
+            if (Input.GetKeyDown(startGameKey))
+            {
+                MoveToDifficulty();
+            }
+
+            if (currentState == MainMenuStates.selectingCharacter)
+            {
+                if (Input.GetKeyDown(enterGameKey))
+                {
+                    activePlayers++;
+                    playerImages[i].color = colors[i];
+                }  
+            }
+            else if (currentState == MainMenuStates.selectingDifficulty)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    difficultyKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + (i + 1).ToString() + "Button" + j.ToString());
+
+                    if (Input.GetKeyDown(difficultyKey))
+                    {
+                        SetDifficulty(j);
+                    }  
+                }
+            } 
+        }           
+    }
+
+    void MoveToDifficulty()
+    {
+        playerSelection.SetActive(false);
+        difficultySelection.SetActive(true);
+        currentState = MainMenuStates.selectingDifficulty;
+    }
+
+    void SetDifficulty(int difficulty)
+    {
+        PlayerPrefs.SetInt("difficulty", difficulty);
+        Application.LoadLevel("Main");
+    }
+}
---
+++ ubi2015_unity/Ubi2015/Assets/GridSizer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GridSizer : MonoBehaviour {
+
+    public GridLayoutGroup grid;
+    public float scaleFactor = 3.805f;
+
+    void Update()
+    {
+        float newSize = Screen.height / scaleFactor;
+        grid.cellSize = new Vector2(newSize, newSize);
+    }
+}
