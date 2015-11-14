a28ff3d8e0e5a486ccc6e4227935d2cd25280027
Flaviu <flaviuvescan@gmail.com>
2015-11-15 01:32:22 +0200

    support for buttons on controller

--- ubi2015_unity/Ubi2015/Assets/Scripts/MovePlayer.cs
+++ ubi2015_unity/Ubi2015/Assets/Scripts/MovePlayer.cs
@@ -11,15 +11,46 @@ public class MovePlayer : MonoBehaviour {
     private string horizName;
     private string verticalName;
 
+    private KeyCode downKey;
+    private KeyCode leftKey;    
+    private KeyCode rightKey;
+    private KeyCode upKey; 
+
     void OnEnable()
     {
         horizName = "Horizontal" + playerID.ToString();
         verticalName = "Vertical" + playerID.ToString();
+
+        downKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + playerID + "Button0");
+        leftKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + playerID + "Button2");
+        rightKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + playerID + "Button1");
+        upKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + playerID + "Button3");
     }
 
     void Update()
     {
         CheckJoysticks();
+        CheckButtons();
+    }
+
+    void CheckButtons()
+    {
+        if (Input.GetKeyDown(downKey))
+        {
+            controller.MoveDown();
+        }
+        if (Input.GetKeyDown(leftKey))
+        {
+            controller.MoveLeft();
+        }
+        if (Input.GetKeyDown(rightKey))
+        {
+            controller.MoveRight();
+        }
+        if (Input.GetKeyDown(upKey))
+        {
+            controller.MoveUp();
+        }
     }
 
     void CheckJoysticks()
