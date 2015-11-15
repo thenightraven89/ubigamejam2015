98d00a830eb6eab17ea21748bf7d1e01c9f5e5eb
Flaviu <flaviuvescan@gmail.com>
2015-11-15 07:34:29 +0200

    victory sound effect

---
+++ ubi2015_unity/Ubi2015/Assets/PlayVictorySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVictorySound : MonoBehaviour {
+
+    public AudioSource source;
+
+    void OnEnable()
+    {
+        StartCoroutine(DelaySound());
+    }
+
+    IEnumerator DelaySound()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        source.Play();
+    }
+}
+
